a0196af6c68006c6088fd1134047c73a82e795bb
ryusenkei <141559610+mr-swedishfish@users.noreply.github.com>
2024-03-25 13:19:43 -0700

    Update 97percent_of_players_cant_beat_this_game.py

--- 97percent_of_players_cant_beat_this_game/97percent_of_players_cant_beat_this_game.py
+++ 97percent_of_players_cant_beat_this_game/97percent_of_players_cant_beat_this_game.py
@@ -14,28 +14,29 @@ from core.models import PermissionLevel
 # ?magic8ball
 
 DIR = dirname(__file__)
-NONBOZO_ROLE = 1158884021460291594
-HALFBOZO_ROLE = 1158883963817971822
-BOZO_ROLE = 1158883765070856202
-BOZO_STICKERS = [
-    "https://media.discordapp.net/attachments/887963616182145044/1220476218101600336/935D7DC6-3CD4-4F14-BAD9-91E2DD8C3CA3.png?ex=660f140d&is=65fc9f0d&hm=120406e1e1bcf5fee87cbaa584f6b77abf911176b5744e8e9f7e0619eeb9d5af&=&format=webp&quality=lossless&width=655&height=655",
-    "https://media.discordapp.net/attachments/887963616182145044/1220476218562969682/Robin_Heart.png?ex=660f140d&is=65fc9f0d&hm=dff58447ff06e7feec447b2718ba33eab3f45ae173299bf54ec6e0e93376b79f&=&format=webp&quality=lossless&width=480&height=480",
-    "https://media.discordapp.net/attachments/887963616182145044/1220476219355697192/Robin_peek.png?ex=660f140e&is=65fc9f0e&hm=891362027443010a448a39459ec9264534afa661debdcc666f2113ab4d8dc05a&=&format=webp&quality=lossless&width=655&height=655",
-    "https://media.discordapp.net/attachments/887963616182145044/1220476220123123764/Robin_Sip.png?ex=660f140e&is=65fc9f0e&hm=3975149e69d87585ca86672641f6a19e28d60d1d9b20721ba6c04c112cbcc20d&=&format=webp&quality=lossless&width=655&height=655",
-    "https://media.discordapp.net/attachments/887963616182145044/1220476220500738158/robin_pout_sparkles.png?ex=660f140e&is=65fc9f0e&hm=40f051fe3f652555466e90e5132543ffdbff7b78ae7abd0f8f8f9fca9813e298&=&format=webp&quality=lossless&width=655&height=655",
-    "https://media.discordapp.net/attachments/887963616182145044/1220476243762479124/robin_faint.png?ex=660f1413&is=65fc9f13&hm=5fd9f527a306361e3396ceb68c9a7a5a2cb7aa36f67daea605f45177edde5438&=&format=webp&quality=lossless&width=655&height=655",
-    "https://media.discordapp.net/attachments/887963616182145044/1220476217464062052/C32D8906-9290-4CE9-B1F9-AB1A8049B569.png?ex=660f140d&is=65fc9f0d&hm=5bff1c836008805adffbab1018d1caf71638d137daf206970a83fdcf281aee96&=&format=webp&quality=lossless&width=655&height=655",
-    "https://media.discordapp.net/attachments/1184968900660703333/1217097457909895258/robin_wave.png?ex=660c03d6&is=65f98ed6&hm=1bf77296262159a42acbf357eed6cb860ef110586e015bc4897a554bf5875ca7&=&format=webp&quality=lossless&width=655&height=655"
+HETERO_ROLE = 1158884021460291594
+BI_ROLE = 1158883963817971822
+GAY_ROLE = 1158883765070856202
+GAY_STICKERS = [
+    "https://media.discordapp.net/attachments/1212916845921828895/1221913234802741418/Untitled298_20240301003453.png?ex=66144e61&is=6601d961&hm=ae05631c391dfa4f585837d77c5a123300dc80b1f21b883394551a47713b5b95&=&format=webp&quality=lossless&width=655&height=655",
+    "https://media.discordapp.net/attachments/1212916845921828895/1221913234358141018/Untitled305_20240311221125.png?ex=66144e61&is=6601d961&hm=d276836a5b8b0fa8201042dfab47382f6a1156940d357edb595f9f501fe0cda2&=&format=webp&quality=lossless&width=655&height=655",
+    "https://media.discordapp.net/attachments/1212916845921828895/1221913235465175160/Untitled298_20240304223727.png?ex=66144e61&is=6601d961&hm=73ea4fac045b95efac09cfc5dd8184727ded510008fc17291268601e7574e728&=&format=webp&quality=lossless&width=655&height=655",
+    "https://media.discordapp.net/attachments/1212916845921828895/1221913235842924625/IMG_6547.png?ex=66144e61&is=6601d961&hm=f50f07e987fa5fe49eb8152df948b05003a4bf7c0836ddc37166120acfbd54b5&=&format=webp&quality=lossless&width=655&height=655",
+    "https://media.discordapp.net/attachments/1212916845921828895/1221913236182667315/IMG_6548.png?ex=66144e61&is=6601d961&hm=e03750b3d7d42ab71ca0223fbeb8164fdfaf5c2afb8a92769b6b2f4af592f458&=&format=webp&quality=lossless&width=655&height=655",
+    "https://media.discordapp.net/attachments/1212916845921828895/1221913740102992012/IMG_5753.png?ex=66144ed9&is=6601d9d9&hm=4ed2be52b96b89ea29c93d692876935240d7479da7b7047a286b11f5d5b196ee&=&format=webp&quality=lossless&width=655&height=655",
+    "https://media.discordapp.net/attachments/1212916845921828895/1221914022404948098/Untitled326_20240325130926.png?ex=66144f1d&is=6601da1d&hm=4a664a9b6167bfed877aeaece64e630ed6fdd0c10b816a94270f9f5174ceedbf&=&format=webp&quality=lossless&width=450&height=450",
+    "https://media.discordapp.net/attachments/1212916845921828895/1221913739478175754/IMG_5755.png?ex=66144ed9&is=6601d9d9&hm=ab8522b3901089dc54f04c17df52fbf672d2932e6948fe55c8d170ca093195e8&=&format=webp&quality=lossless&width=655&height=655",
+    "https://media.discordapp.net/attachments/1212916845921828895/1221913740614832148/IMG_5756.png?ex=66144ed9&is=6601d9d9&hm=af8db425af79a16ebd6f23591dff118ffdef7874af3f2e79f947a2f694bdfed6&=&format=webp&quality=lossless&width=655&height=655"
 ]
 
 EIGHT_BALL_TITLES = [
-    'Robin has thought about this...',
-    'Robin has decided...',
-    'Robin has consulted Xipe...',
-    'Robin has set aside some time to think about this...',
-    'Robin has concluded...',
-    'Robin has gathered some information...',
-    'Robin has inferred...'
+    'Jade has thought about this...',
+    'Jade has decided...',
+    'Jade has consulted the IPC...',
+    'Jade has set aside some time to think about this...',
+    'Jade has concluded...',
+    'Jade has gathered some information...',
+    'Jade has inferred...'
 ]
 
 class HahaFunny(commands.Cog):
@@ -53,12 +54,12 @@ class HahaFunny(commands.Cog):
         num = random.randrange(10001) / 100
 
         embed = discord.Embed(
-            title=f"Robin has measured the odds...",
+            title=f"Jade has calculated the odds...",
             colour=discord.Colour.random()
         )
         embed.add_field(name='Event', value=text)
         embed.add_field(name="Probability", value=f"This event is **{num}%** likely.", inline=False)
-        embed.set_thumbnail(url=random.choice(BOZO_STICKERS))
+        embed.set_thumbnail(url=random.choice(GAY_STICKERS))
 
         await ctx.send(embed=embed)
 
@@ -76,16 +77,16 @@ class HahaFunny(commands.Cog):
             ans = json.load(f)
 
         if num < 3:
-            thumbnail = "https://media.discordapp.net/attachments/887963616182145044/1220476243762479124/robin_faint.png?ex=660f1413&is=65fc9f13&hm=5fd9f527a306361e3396ceb68c9a7a5a2cb7aa36f67daea605f45177edde5438&=&format=webp&quality=lossless&width=655&height=655"
-            emote = discord.utils.get(ctx.guild.emojis, id=1211059820698796052)
+            thumbnail = "https://media.discordapp.net/attachments/1212916845921828895/1221914022404948098/Untitled326_20240325130926.png?ex=66144f1d&is=6601da1d&hm=4a664a9b6167bfed877aeaece64e630ed6fdd0c10b816a94270f9f5174ceedbf&=&format=webp&quality=lossless&width=450&height=450"
+            emote = discord.utils.get(ctx.guild.emojis, id=1214461577118490695)
             answer = random.choice(ans[2]["negative"])
         elif num < 6:
-            thumbnail = "https://media.discordapp.net/attachments/887963616182145044/1220476220123123764/Robin_Sip.png?ex=660f140e&is=65fc9f0e&hm=3975149e69d87585ca86672641f6a19e28d60d1d9b20721ba6c04c112cbcc20d&=&format=webp&quality=lossless&width=655&height=655"
-            emote = discord.utils.get(ctx.guild.emojis, id=1201386255049564200)
+            thumbnail = "https://media.discordapp.net/attachments/1212916845921828895/1221913235465175160/Untitled298_20240304223727.png?ex=66144e61&is=6601d961&hm=73ea4fac045b95efac09cfc5dd8184727ded510008fc17291268601e7574e728&=&format=webp&quality=lossless&width=655&height=655"
+            emote = discord.utils.get(ctx.guild.emojis, id=1219369970916397106)
             answer = random.choice(ans[1]["neutral"])
         elif num < 10:
-            thumbnail = "https://media.discordapp.net/attachments/887963616182145044/1220476218562969682/Robin_Heart.png?ex=660f140d&is=65fc9f0d&hm=dff58447ff06e7feec447b2718ba33eab3f45ae173299bf54ec6e0e93376b79f&=&format=webp&quality=lossless&width=480&height=480"
-            emote = discord.utils.get(ctx.guild.emojis, id=1211077629499805788)
+            thumbnail = "https://media.discordapp.net/attachments/1212916845921828895/1221913236182667315/IMG_6548.png?ex=66144e61&is=6601d961&hm=e03750b3d7d42ab71ca0223fbeb8164fdfaf5c2afb8a92769b6b2f4af592f458&=&format=webp&quality=lossless&width=655&height=655"
+            emote = discord.utils.get(ctx.guild.emojis, id=1214461477906550854)
             answer = random.choice(ans[0]["positive"])
         else:  # Easter egg
             thumbnail = "https://s3.blankdvth.com/74b72448-f31f-4d85-a765-fa04bca84edd.jpg"
@@ -93,7 +94,7 @@ class HahaFunny(commands.Cog):
             answer = f"You've won, you've done the impossible. Contact the bot devs to see them become confused. (`{num}`)"
 
         embed = discord.Embed(
-            title=f"Robin is vibe checking...",
+            title=f"Jade is vibe checking...",
             description=f"{emote} {member.display_name} {answer}",
             colour=discord.Colour.random()
         )
@@ -101,35 +102,35 @@ class HahaFunny(commands.Cog):
         
         await ctx.send(embed=embed)
     
-    # Bozo Meter
+    # Gaydar
     @checks.has_permissions(PermissionLevel.REGULAR)
-    @commands.command(aliases=['bozometer', 'bozorate'])
-    async def bozo(self, ctx: commands.Context, member: commands.MemberConverter = None):
-        """Are you a bozo?"""
+    @commands.command(aliases=['gae', 'gayrate', 'gaydar'])
+    async def gay(self, ctx: commands.Context, member: commands.MemberConverter = None):
+        """Why are you gae?"""
         if member is None:
             member = ctx.author
 
         num = random.randrange(10001) / 100
 
         embed = discord.Embed(
-            title=f"The Family has decided that...",
-            description=f"{member.display_name} is **{num}%** bozo.",
+            title=f"The IPC has decided that...",
+            description=f"{member.display_name} is **{num}%** gay.",
             colour=discord.Colour.random()
         )
-        embed.set_thumbnail(url=random.choice(BOZO_STICKERS))
+        embed.set_thumbnail(url=random.choice(gay_STICKERS))
 
         # funi footer if anyone gets either
         if num == 0:
-            embed.set_footer(text=f'[{member.display_name} is now a Certified Non-Bozo]')
-            role = discord.utils.get(ctx.guild.roles, id=NONBOZO_ROLE)
+            embed.set_footer(text=f'[{member.display_name} is now a Certified Hetero.]')
+            role = discord.utils.get(ctx.guild.roles, id=HETERO_ROLE)
             await member.add_roles(role)
         elif num == 50:
-            embed.set_footer(text=f'[{member.display_name} is now a Certified Half Bozo]')
-            role = discord.utils.get(ctx.guild.roles, id=HALFBOZO_ROLE)
+            embed.set_footer(text=f'[{member.display_name} is now a Certified Bi.]')
+            role = discord.utils.get(ctx.guild.roles, id=BI_ROLE)
             await member.add_roles(role)
         elif num == 100:
-            embed.set_footer(text=f'[{member.display_name} is now a Certified Bozo. LAUGH AT THIS USER!!]')
-            role = discord.utils.get(ctx.guild.roles, id=BOZO_ROLE)
+            embed.set_footer(text=f'[{member.display_name} is now a Certified Gay. BE WHO YOU ARE FOR YOUR PRIDE!!]')
+            role = discord.utils.get(ctx.guild.roles, id=GAY_ROLE)
             await member.add_roles(role)
             
         await ctx.send(embed=embed)
@@ -138,7 +139,7 @@ class HahaFunny(commands.Cog):
     @checks.has_permissions(PermissionLevel.REGULAR)
     @commands.command(aliases=['8ball', 'ball'])
     async def magic8ball(self, ctx: commands.Context, *, text: str):
-        """Ask Robin a "Yes or No" question~"""
+        """Ask Jade a "Yes or No" question~"""
 
         num = random.randint(0, 9)
 
@@ -146,16 +147,16 @@ class HahaFunny(commands.Cog):
             ans = json.load(f)
 
         if num < 3:
-            thumbnail = "https://media.discordapp.net/attachments/887963616182145044/1220476243762479124/robin_faint.png?ex=660f1413&is=65fc9f13&hm=5fd9f527a306361e3396ceb68c9a7a5a2cb7aa36f67daea605f45177edde5438&=&format=webp&quality=lossless&width=655&height=655"
-            emote = discord.utils.get(ctx.guild.emojis, id=1211059820698796052)
+            thumbnail = "https://media.discordapp.net/attachments/1212916845921828895/1221913739478175754/IMG_5755.png?ex=66144ed9&is=6601d9d9&hm=ab8522b3901089dc54f04c17df52fbf672d2932e6948fe55c8d170ca093195e8&=&format=webp&quality=lossless&width=655&height=655"
+            emote = discord.utils.get(ctx.guild.emojis, id=1216443083605020732)
             answer = random.choice(ans[2]["negative"])
         elif num < 6:
-            thumbnail = "https://media.discordapp.net/attachments/887963616182145044/1220476219355697192/Robin_peek.png?ex=660f140e&is=65fc9f0e&hm=891362027443010a448a39459ec9264534afa661debdcc666f2113ab4d8dc05a&=&format=webp&quality=lossless&width=655&height=655"
-            emote = discord.utils.get(ctx.guild.emojis, id=1193067844066344991)
+            thumbnail = "https://media.discordapp.net/attachments/1212916845921828895/1221913235465175160/Untitled298_20240304223727.png?ex=66144e61&is=6601d961&hm=73ea4fac045b95efac09cfc5dd8184727ded510008fc17291268601e7574e728&=&format=webp&quality=lossless&width=655&height=655"
+            emote = discord.utils.get(ctx.guild.emojis, id=1214461527784951838)
             answer = random.choice(ans[1]["neutral"])
         elif num < 10:
-            thumbnail = "https://media.discordapp.net/attachments/887963616182145044/1220476218101600336/935D7DC6-3CD4-4F14-BAD9-91E2DD8C3CA3.png?ex=660f140d&is=65fc9f0d&hm=120406e1e1bcf5fee87cbaa584f6b77abf911176b5744e8e9f7e0619eeb9d5af&=&format=webp&quality=lossless&width=655&height=655"
-            emote = discord.utils.get(ctx.guild.emojis, id=1200472301611778172)
+            thumbnail = "https://media.discordapp.net/attachments/1212916845921828895/1221913740102992012/IMG_5753.png?ex=66144ed9&is=6601d9d9&hm=4ed2be52b96b89ea29c93d692876935240d7479da7b7047a286b11f5d5b196ee&=&format=webp&quality=lossless&width=655&height=655"
+            emote = discord.utils.get(ctx.guild.emojis, id=1216133118067609660)
             answer = random.choice(ans[0]["positive"])
         else:  # Easter egg
             thumbnail = "https://s3.blankdvth.com/74b72448-f31f-4d85-a765-fa04bca84edd.jpg"
