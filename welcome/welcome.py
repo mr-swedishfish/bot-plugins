a32cf2099000f7daef7f04e943a79f1a857cff6e
ryusenkei <141559610+mr-swedishfish@users.noreply.github.com>
2024-04-06 21:05:43 -0700

    Update welcome.py

--- welcome/welcome.py
+++ welcome/welcome.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 
 
-CHANNEL_ID = 1106794799199174686
+CHANNEL_ID = 1191916301250203766
 
 
 class Welcome(commands.Cog):
@@ -15,16 +15,16 @@ class Welcome(commands.Cog):
         channel = self.bot.get_channel(CHANNEL_ID)
         if channel is not None:
             embed = discord.Embed(
-                title=f"Welcome to Ruan Mei Mains, {member.name}!",
-                description=f"Please read <#1106786003777224804> to avoid any trouble, and <#1106794848117342280> to familiarize yourself with the server.\n\n We wish you a pleasant stay; if you need help, DM <@1148374427441049750> to start a Modmail ticket!",
-                colour=discord.Colour.from_rgb(84, 140, 140)
+                title=f"Welcome to Jade Mains, {member.name}!",
+                description=f"Please read <#1191916301250203767> to verify and avoid any trouble, and <#1191916301250203768> to grab your roles.\n\n We wish you a pleasant stay; if you need help, DM <@1219772651216703498> to start a Modmail ticket!",
+                colour=discord.Colour.from_rgb(237, 159, 236)
             )
 
             embed.set_thumbnail(url=member.display_avatar)
             embed.set_image(
-                url='https://cdn.discordapp.com/attachments/1099432790229004439/1153915275591294987/Ruan-Mei-Honkai-Star-Rail.png')
+                url='https://media.discordapp.net/attachments/1223541148815982613/1226381920112480346/image.png?ex=6624902a&is=66121b2a&hm=c397086d5027b4028df7482297c5f4d81fbb64c26286bd754735fdfc4eaf7474&=&format=webp&quality=lossless&width=1035&height=655')
             embed.set_footer(text=f'Thanks to you, we now have {guild.member_count} members!',
-                             icon_url='https://cdn.discordapp.com/attachments/1106792127268139119/1153168727701995620/ruanmei_flower.png')
+                             icon_url='https://media.discordapp.net/attachments/1213247217792712764/1226378586945224745/JadeCash.png?ex=66248d10&is=66121810&hm=8cfd6469f23c201439908d8eaea14e8a663521701919ae7627ca6afb33ea42bf&=&format=webp&quality=lossless&width=636&height=655')
 
             await channel.send(content=member.mention, embed=embed)
 
