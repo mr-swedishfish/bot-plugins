c9d1159fdd33cdab793a074d06903169d7bce491
ryusenkei <141559610+mr-swedishfish@users.noreply.github.com>
2024-04-06 21:06:08 -0700

    Update welcome.py

--- welcome/welcome.py
+++ welcome/welcome.py
@@ -22,7 +22,7 @@ class Welcome(commands.Cog):
 
             embed.set_thumbnail(url=member.display_avatar)
             embed.set_image(
-                url='https://media.discordapp.net/attachments/1223541148815982613/1226381920112480346/image.png?ex=6624902a&is=66121b2a&hm=c397086d5027b4028df7482297c5f4d81fbb64c26286bd754735fdfc4eaf7474&=&format=webp&quality=lossless&width=1035&height=655')
+                url='https://media.discordapp.net/attachments/1213247217792712764/1226382356248531005/Untitled315_20240327175233.png?ex=66249092&is=66121b92&hm=aff0032e92228b4996d4c558b916527905b67833901a02502a87ed9a600b1fa0&=&format=webp&quality=lossless&width=1440&height=654')
             embed.set_footer(text=f'Thanks to you, we now have {guild.member_count} members!',
                              icon_url='https://media.discordapp.net/attachments/1213247217792712764/1226378586945224745/JadeCash.png?ex=66248d10&is=66121810&hm=8cfd6469f23c201439908d8eaea14e8a663521701919ae7627ca6afb33ea42bf&=&format=webp&quality=lossless&width=636&height=655')
 
