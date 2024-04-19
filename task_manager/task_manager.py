3e0de785a8350ca3fdcd1c99117e38799b3e355c
ryusenkei <141559610+mr-swedishfish@users.noreply.github.com>
2024-04-19 08:03:50 -0700

    Update task_manager.py

--- task_manager/task_manager.py
+++ task_manager/task_manager.py
@@ -3,8 +3,23 @@ import os
 from discord.ext import commands
 bot = discord.Bot()
 
-@bot.slash_command()
+task = bot.create_group("create")
+
+@task.command()
 async def modal_slash(ctx: discord.ApplicationContext):
     """Shows an example of a modal dialog being invoked from a slash command."""
     modal = MyModal(title="Modal via Slash Command")
     await ctx.send_modal(modal)
+
+class MyModal(discord.ui.Modal):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+        self.add_item(discord.ui.InputText(label="Short Input"))
+        self.add_item(discord.ui.InputText(label="Long Input", style=discord.InputTextStyle.long))
+
+    async def callback(self, interaction: discord.Interaction):
+        embed = discord.Embed(title="Modal Results")
+        embed.add_field(name="Short Input", value=self.children[0].value)
+        embed.add_field(name="Long Input", value=self.children[1].value)
+        await interaction.response.send_message(embeds=[embed])
